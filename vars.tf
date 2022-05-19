73e5a1138b24d4558e093420c80497400e9123a0
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-19 06:42:25 +0530

    Update terraform/tf-module-immutable-app/

--- vars.tf
+++ vars.tf
@@ -4,6 +4,8 @@ variable "APP_PORT" {}
 variable "APP_VERSION" {}
 variable "APP_TYPE" {}
 variable "INSTANCE_TYPE" {}
+variable "WORKSTATION_IP" {}
+variable "PROMETHEUS_IP" {}
 
 variable "DESIRED_CAPACITY" {}
 variable "MIN_SIZE" {}
